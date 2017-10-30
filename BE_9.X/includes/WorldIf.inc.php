f726594686b5456dd1a17f544151c64ed75c395c
JecraDK <Jesperuthkrab@hotmail.com>
2017-10-30 12:26:25 +0100

    Revert "BE9.X Upload"
    This reverts commit a2c070c47c7f1320308d16c604e4b829533c425c.

--- BE_9.X/includes/WorldIf.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//
-// Author : Jesper Uth Krab
-// Made On : Oct 4, 2017 1:59:06 PM  
-//
-
-error_reporting(E_ALL);
--- BE_9.X/includes/WorldView1.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//
-// Author : Jesper Uth Krab
-// Made On : Oct 4, 2017 1:57:59 PM  
-//
-
-error_reporting(E_ALL);
