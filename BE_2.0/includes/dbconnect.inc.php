37d5519c9fb21c3ed8844c835312e810bb5541d8
JecraDK <Jesperuthkrab@hotmail.com>
2017-10-30 12:08:49 +0100

    BE2.0 Upload

---
+++ BE_2.0/includes/dbconnect.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+//
+// Author : Jesper Uth Krab
+// Made On : Sep 13, 2017 3:29:40 PM  
+//
+
+error_reporting(E_ALL);
+
+try {
+    $dbh = new PDO(DSN, DBUSER, USERPWD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    printf("<p>Connect failed for following reason: <br/>%s</p>\n",
+        $e->getMessage());
+}
