be7a868e7e7c7c651b124b7b64b39ddb690ce2de
JecraDK <Jesperuthkrab@hotmail.com>
2017-11-03 11:06:58 +0100

    Add files via upload

---
+++ SP_1.X/assSP11.sql
@@ -0,0 +1,65 @@
+
+create table t3 (
+    
+	dist int(11) not null auto_incremented,
+    
+	repno int(11) not null,
+    
+	repname varchar(16) not null,
+    
+	primary key (dist)
+
+);
+
+
+
+create table t4 (
+    
+	cust int(11) not null auto_incremented,
+    
+	custname varchar(16) not null,
+    
+	district int(11) not null,
+    
+	revenue float not null,
+    
+	primary key (cust),
+    
+	index (disctrict)
+
+);
+
+
+
+create procedure p3 ()
+    
+begin
+        
+	declare v int;
+        
+	set v = 0;
+        
+	while v < 5 do
+            
+		insert into t3 (repno, repname) 
+		values (rand(),'text');
+            
+		set v = v + 1;
+        
+	end while;
+    
+end;
+
+
+
+create procedure p4 ()
+    
+begin
+        
+	while t4 (district) < 5 do
+            
+		insert into t4 (cust) values (10000);
+        
+	end while;
+    
+end;
---
+++ SP_1.X/assign100.sql
@@ -0,0 +1,47 @@
+
+create table t2 (
+    
+	pk1 int not null auto_incremented,
+    
+	visited date not null,
+    
+	named varchar(32) not null,
+    
+	num float not null
+    
+	primary key (pk1)
+
+);
+
+
+
+create procedure p1 ()
+    
+begin
+        
+	set @named = Jesper;
+        
+	set @visited = now();
+        
+	set @num = rand();
+        
+	insert into t2 values (@named, @visited, @num);
+    
+
+
+
+create procedure p2 (p int)
+    
+	select *
+ from t2
+    
+	where pk1 = p
+
+call p2(1)
+
+call p2(2)
+
+call p2(3)
+call p2(4)
+
+call p2(5)
